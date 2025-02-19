b4d6a7cab24096eff32d51f0b53578f02a5ccde8
橋本三琴 <mikoto@hashimotosankinnoMac-mini.local>
2025-02-19 21:25:54 +0900

    3rd commit

---
+++ my_project/target/compiled/my_project/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from DBT_DB.DBT_SCHEMA.my_first_dbt_model
+where id is null
+
+
---
+++ my_project/target/compiled/my_project/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from DBT_DB.DBT_SCHEMA.my_second_dbt_model
+where id is null
+
+
